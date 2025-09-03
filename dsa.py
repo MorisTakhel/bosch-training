d0293c9bb2d084b65578cbf2df2103849ab7c4b5
Moris <moristakhel@gmail.com>
2025-09-03 10:22:29 +0530

    Added queue

--- dsa.py
+++ dsa.py
@@ -70,3 +70,26 @@ class StackLL:
             print(currentNode.value, end =" -> ")
             currentNode = currentNode.next
         print()
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, element):
+        self.queue.append(element)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "Queue is empty"
+        return self.queue.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            return "Queue is empty"
+        return self.queue[0]
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def size(self):
+        return len(self.queue)
